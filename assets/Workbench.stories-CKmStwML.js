5b78e511427fcf612741b33914d55c05cc66ac1c
joyz0 <joyz0@users.noreply.github.com>
2024-04-07 16:06:44 +0000

    deploy: 8880d40794b018035fdc4b1aef320c4f84270945

--- assets/Workbench.stories-CKmStwML.js
+++
@@ -1,5 +0,0 @@
-import{i,j as o,I as c,E as p}from"./IoC-Rx6VvmpO.js";import{r as t}from"./index-BBkUAzwr.js";const u=i();function m(){const r=t.useRef(null);return t.useEffect(()=>{r.current&&u.get(c.Workbench).initialize({container:r.current,initialPosition:p.BOTTOM,panelSize:30})},[]),o.jsx("div",{ref:r})}const E={title:"Example/Workbench",tags:["autodocs"],parameters:{layout:"fullscreen"}},e={render(){return o.jsx(m,{})}};var n,s,a;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  render() {
-    return <Wrapper />;
-  }
-}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const x=["Demo"];export{e as Demo,x as __namedExportsOrder,E as default};
